f2226b29f750c5071c7f8f2b38e2f0a289ee9e71
Madina192 <madinaakpaeva000@gmail.com>
2023-05-02 23:06:46 +0600

    added carousel

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,18 +2,32 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import logo from "../img/logo.jpg";
 import "./Header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
         <div>
             <header className="header">
-                <img className='header-logo' src={logo} alt=""/>
+                <Link to="/">
+                    <img className='header-logo' src={logo} alt=""/>
+                </Link>
+                <h1 className="h1">Ala-Too Shop</h1>
                 <nav className='nav'>
-                    <Link className='nav-link' exact='true' to='/home'>Home</Link>
-                    <Link className='nav-link' to='/cart'>Cart</Link>
-                    <Link className='nav-link' to='/product'>Cart</Link>
-                    <Link className='nav-link' to='/payment'>Cart</Link>
+                    <Link className='nav-link' to='/cart'>
+                        <FontAwesomeIcon icon={faCartShopping} />
+                    </Link>
+                    <Link className='nav-link' to='/profile'>
+                        <FontAwesomeIcon icon={faUser} />
+                    </Link>
                 </nav>
+                <form action="" className="form_lang">
+                    <select id="lang" name="lang" className="select_lang">
+                        <option value="en" className="option">English</option>
+                        <option value="ru" className="option">Русский</option>
+                    </select>
+                </form>
             </header>
         </div>
     );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,86 @@
 import React from 'react';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import img from "../../components/img/img.png"
+import img1 from "../../components/img/img_1.png"
+import img2 from "../../components/img/img_2.png"
+import img3 from "../../components/img/img_3.png"
+import img4 from "../../components/img/img_4.png"
+import img5 from "../../components/img/img_5.png"
+import img6 from "../../components/img/img_6.png"
+import img7 from "../../components/img/img_7.png"
+import img8 from "../../components/img/img_8.png"
 
 const Home = () => {
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 1
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
     return (
         <div>
-            <div>
-                hellooooo
+            <div className="carousel_box">
+                    <Carousel
+                        responsive={responsive}
+                        additionalTransfrom={0}
+                        arrows
+                        autoPlay={true}
+                        autoPlaySpeed={2000}
+                        centerMode={false}
+                        className='carousel'
+                        containerClass="container-with-dots"
+                        dotListClass=''
+                        draggable
+                        focusOnSelect={true}
+                        infinite
+                        itemClass=''
+                        keyBoardControl
+                        minimumTouchDrag={80}
+                        pauseOnHover
+                        renderArrowsWhenDisabled={false}
+                        renderButtonGroupOutside={false}
+                        renderDotsOutside={false}
+                    >
+                        <div className="carousel_min_box">
+                            <div className="carousel_text">
+                                spring discount 10%
+                            </div>
+                            <div className="carousel_img_box">
+                                <img src={img} alt="" className="carousel_img"/>
+                                <img src={img1} alt="" className="carousel_img"/>
+                                <img src={img2} alt="" className="carousel_img"/>
+                            </div>
+                        </div>
+                        <div className="carousel_min_box">
+                            <div className="carousel_text">
+                                spring discount 10%
+                            </div>
+                            <img src={img3} alt="" className="carousel_img"/>
+                            <img src={img4} alt="" className="carousel_img"/>
+                            <img src={img5} alt="" className="carousel_img"/>
+                        </div>
+                        <div className="carousel_min_box">
+                            <div className="carousel_text">
+                                spring discount 10%
+                            </div>
+                            <img src={img6} alt="" className="carousel_img"/>
+                            <img src={img7} alt="" className="carousel_img"/>
+                            <img src={img8} alt="" className="carousel_img"/>
+                        </div>
+                    </Carousel>
             </div>
         </div>
     );
